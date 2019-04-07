151fd8b0240b31ef4b4981bb79d8d5b0d32b54d0
vivekchandrabs <vivek.chandra.301096@gmail.com>
2019-04-07 15:42:52 +0530

    Add js and css file

--- webserver/static/index.js
+++ webserver/static/index.js
@@ -1,1 +1,35 @@
-alert("Im in");
+var logdiv = $('#moodlog');
+
+console.log(logdiv);
+
+$('btn').click(function(){
+
+
+
+});
+
+
+
+
+$.ajax({
+
+	method: 'GET',
+	url: 'http://127.0.0.1:8000/api/logs/',
+
+	success: function(response){
+
+		for (let moodl of response){
+			
+			logdiv.append(`
+				<h3>${moodl.note}</h3>
+
+				<span class='tag'>Mood: ${moodl.mood.emoji} ${moodl.mood.title}</span>
+				<span class='tag'>Action: ${moodl.action.emoji} ${moodl.action.title}</span>
+				<hr>
+				`);
+
+		}
+
+		}
+
+});
