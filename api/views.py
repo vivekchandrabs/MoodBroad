29010504607aa20d1eb0711433c75c0086df3cba
vivekchandrabs <vivek.chandra.301096@gmail.com>
2019-04-07 12:22:39 +0530

    add response and request

--- api/views.py
+++ api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from api.models import Mood, Action, MoodLog
 
-# Create your views here.
+class GetMoods(APIView):
+
+	def get(self, request):
+
+		moods = Mood.objects.all().values('id', 'title', 'emoji')
+		print(moods)
+		moods = list(moods)
+		print(moods)
+
+		data = json.dumps(moods)
+
+		return Response(data)
